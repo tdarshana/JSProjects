56e001bd063f04faad8d7b95b21f70dd962bd697
tharindud <tharindud@intervest.lk>
2019-04-02 17:50:22 +0530

    Completed the Maze.

--- PacMan/Maze/Maze.js
+++ PacMan/Maze/Maze.js
@@ -30,31 +30,35 @@ maze = [
         new Brick(new Movement(1,1,0,0), 10, false, false)
     ],
     [
-        new Brick(new Movement(0,1,1,0), 10, false, false), 
-        new Brick(new Movement(0,1,1,0), 10, false, false), 
-        new Brick(new Movement(0,1,1,0), 10, false, false), 
-        new Brick(new Movement(0,1,1,0), 10, false, false), 
-        new Brick(new Movement(0,1,1,0), 10, false, false)
+        new Brick(new Movement(1,1,0,0), 10, false, false), 
+        new Brick(new Movement(1,1,0,0), 10, false, false), 
+        new Brick(new Movement(1,1,1,0), 0, false, false), 
+        new Brick(new Movement(0,0,1,1), 10, false, false), 
+        new Brick(new Movement(1,1,0,1), 10, false, false),
+        new Brick(new Movement(1,1,0,0), 10, false, false)
     ],
     [
+        new Brick(new Movement(1,1,0,0), 10, false, false), 
+        new Brick(new Movement(1,1,0,0), 10, false, false), 
+        new Brick(new Movement(1,1,0,0), 10, false, false), 
         new Brick(new Movement(0,1,1,0), 10, false, false), 
-        new Brick(new Movement(0,1,1,0), 10, false, false), 
-        new Brick(new Movement(0,1,1,0), 10, false, false), 
-        new Brick(new Movement(0,1,1,0), 10, false, false), 
-        new Brick(new Movement(0,1,1,0), 10, false, false)
+        new Brick(new Movement(1,1,0,1), 10, false, false),
+        new Brick(new Movement(1,1,0,0), 10, false, false)
     ],
     [
-        new Brick(new Movement(0,1,1,0), 10, false, false), 
-        new Brick(new Movement(0,1,1,0), 10, false, false), 
-        new Brick(new Movement(0,1,1,0), 10, false, false), 
-        new Brick(new Movement(0,1,1,0), 10, false, false), 
-        new Brick(new Movement(0,1,1,0), 10, false, false)
+        new Brick(new Movement(1,1,1,0), 10, false, false), 
+        new Brick(new Movement(1,0,1,1), 10, false, false), 
+        new Brick(new Movement(1,0,1,1), 10, false, false), 
+        new Brick(new Movement(1,0,0,1), 10, false, false), 
+        new Brick(new Movement(1,0,1,0), 10, false, false),
+        new Brick(new Movement(1,1,0,1), 10, false, false)
     ],
     [
-        new Brick(new Movement(0,1,1,0), 10, false, false), 
-        new Brick(new Movement(0,1,1,0), 10, false, false), 
-        new Brick(new Movement(0,1,1,0), 10, false, false), 
-        new Brick(new Movement(0,1,1,0), 10, false, false), 
-        new Brick(new Movement(0,1,1,0), 10, false, false)
+        new Brick(new Movement(1,0,1,0), 10, false, false), 
+        new Brick(new Movement(0,0,1,1), 10, false, false), 
+        new Brick(new Movement(0,0,1,1), 10, false, false), 
+        new Brick(new Movement(0,0,1,1), 10, false, false), 
+        new Brick(new Movement(0,0,1,1), 10, false, false), 
+        new Brick(new Movement(1,0,0,1), 10, false, false)
     ]
 ];
